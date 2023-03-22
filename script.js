3909cbf1bd26bba198eae0286337f1c18eaf6433
donlHenard <pastor.git101@gmail.com>
2023-03-22 15:17:18 +0800

    Add a function called removePixel
    Basically removes all the child element of canvass div. It uses while
    loop as it runs as long as it has child

--- script.js
+++ script.js
@@ -2,26 +2,41 @@ const main = document.querySelector('#canvass');
 
 function grid (x = 16) {
 
-    for(let i = 1; i <= x; i++){
-        const row = document.createElement('div');
-        row.classList.add('row')
-        main.appendChild(row);
-
-        for(let j = 1; j <= x; j++) {
-            const pixel = document.createElement('div');
-            pixel.addEventListener('mouseover', () =>  {
-                pixel.style.backgroundColor = 'black';
-            });
-            pixel.classList.add('pixel');
-            row.appendChild(pixel);
+    if( x > 100){
+        alert(`Computer can't process more than 100x100 pixels`);
+    } else {
+        for(let i = 1; i <= x; i++){
+            const row = document.createElement('div');
+            row.classList.add('row')
+            main.appendChild(row);
+    
+            for(let j = 1; j <= x; j++) {
+                const pixel = document.createElement('div');
+                pixel.addEventListener('mouseover', () =>  {
+                    pixel.style.backgroundColor = 'black';
+                });
+                pixel.classList.add('pixel');
+                row.appendChild(pixel);
+            }
         }
+    }
+}
+
+function removePixel(canvass) {
+    while (canvass.firstChild) {
+        canvass.removeChild(canvass.firstChild);
     }
 }
 
 const button = document.querySelector('button');
 button.addEventListener('click', () => {
-    let number = prompt('How many square per side?');
-    grid(number);
+    let number = prompt('How many square per side? Max - 100');
+    if(number == null || number == ""){
+        alert("You canceled");
+    } else {
+        removePixel(main);
+        grid(number);
+    }
 });
 
 grid();
